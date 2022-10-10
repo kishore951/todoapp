58cabbb14867d3d1d6316ed9ad68fb80e1edfc63
kishore951 <kishoresakthivel951@gmail.com>
2022-10-10 13:50:35 +0530

    success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
+const appRoutes:Routes=[
+  {
+    path:"",component:TodoListComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +20,8 @@ import { TodoListComponent } from './todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
